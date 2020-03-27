5a0d8c367734296c5423d6333d86109e1e321a71
Bulent Coskun <bcoskun@conveyhs.com>
2020-03-27 09:11:59 -0400

    windows related fixes

---
+++ winbuild.bat
@@ -0,0 +1,5 @@
+SET GOPATH=C:\Users\BCoskun\krakend-ce\go
+go get .
+attrib -R C:\Users\BCoskun\krakend-ce\go\pkg\mod\github.com\devopsfaith\krakend-gologging@v0.0.0-20190131142345-f3f256584ecc\log.go
+copy C:\Users\BCoskun\krakend-ce\winbuild\deopsfaith_krakend-gologging_log.go C:\Users\BCoskun\krakend-ce\go\pkg\mod\github.com\devopsfaith\krakend-gologging@v0.0.0-20190131142345-f3f256584ecc\log.go
+go build -ldflags="-X github.com/devopsfaith/krakend/core.KrakendVersion=1.1.1" -o krakend-ce.exe ./cmd/krakend-ce
